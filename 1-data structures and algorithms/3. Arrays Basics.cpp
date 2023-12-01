5487f96375beb2daaa9ca21f10b13e18237fabf9
3bHussein <39864308+3bHussein@users.noreply.github.com>
2023-12-01 04:43:39 +0400

    .

---
+++ 1-data structures and algorithms/3. Arrays Basics.cpp
@@ -0,0 +1,18 @@
+// #include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+   printf("Hello, World!");
+
+   int A[5];
+   A[0] = 5;
+   int f=0;
+   cout<<A[0];
+
+
+   return 0;
+}
