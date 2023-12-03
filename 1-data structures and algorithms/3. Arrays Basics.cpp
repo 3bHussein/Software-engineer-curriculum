0bc8b6f67ec8e57de2c48dc5b7ef10d9f18e8cf2
3bHussein <39864308+3bHussein@users.noreply.github.com>
2023-12-03 23:55:39 +0400

    /

--- 1-data structures and algorithms/3. Arrays Basics.cpp
+++ 1-data structures and algorithms/3. Arrays Basics.cpp
@@ -6,13 +6,32 @@ using namespace std;
 int main()
 {
 
-   printf("Hello, World!");
 
+// fixed array
    int A[5];
    A[0] = 5;
-   int f=0;
-   cout<<A[0];
+   A[1] =54;
+   A[2]= 54;
+
+   // cout<<sizeof(A)<<endl;
+   // cout<<A[0];
+
+// dynamic array 
+   // int B[];
+   int B[]={34,5,6};
+   // cout<<B[1]<<endl;
+   // cout<<sizeof(B)<<endl;
 
 
+// for loop 
+
+   
+   for (size_t i = 0; i < 6; i++)
+   {
+    
+    cout<<B[i]<<endl;
+   //  cout<<B[i]<<endl;
+   }
+   
    return 0;
 }
