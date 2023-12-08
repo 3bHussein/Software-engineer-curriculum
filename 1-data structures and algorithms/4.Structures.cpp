2346d1cb89c0f4669a38b49111904e86cab9f557
3bHussein <39864308+3bHussein@users.noreply.github.com>
2023-12-08 08:09:55 +0400

    /

--- 1-data structures and algorithms/4.Structures.cpp
+++ 1-data structures and algorithms/4.Structures.cpp
@@ -4,14 +4,31 @@
 
 using namespace std;
 
+// Structures
+// example Rectangle
+// how to defined Structures
+struct Rectangle
+{
+     int length ;
+    int breadth ;
+    
+};
+
 int main()
 {
+// to declare  Struct
+// struct Rectangle R;
+// to declare and initialize 
+struct Rectangle R ={.length=4,.breadth=6};
 
- int d =4;
- cout<<d;
+cout<<R.breadth;
 
+int Total =R.breadth*R.length;
+// corrupt data
+R.breadth=5;
+R.length=6;
 
-
+cout<<Total<<endl;
 
    return 0;
 }
