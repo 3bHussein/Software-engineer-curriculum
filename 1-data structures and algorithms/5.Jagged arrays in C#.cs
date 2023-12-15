9528404acf338208cc40dc15b70a61dfda0c142d
3bHussein <39864308+3bHussein@users.noreply.github.com>
2023-12-15 19:18:02 +0400

    Update 5.Jagged arrays in C#.cs

--- 1-data structures and algorithms/5.Jagged arrays in C#.cs
+++ 1-data structures and algorithms/5.Jagged arrays in C#.cs
@@ -6,6 +6,19 @@ public static void Main(string[] args)
     jagged[0]=new int[2];
     jagged[0][0]=8;
     jagged[0][1]=6;
+//  set row 1
+    jagged[1]=new int[2];
+    jagged[1][0]=18;
+    jagged[1][1]=61;
+//  set row 3
+    jagged[3]=new int[2];
+    jagged[3][0]=83;
+    jagged[3][1]=63;
+
+
+
+    Console.WriteLine(jagged[3]);
+
 
 
 
